4a2b88f0be27eeb19eef1e7066ba47ced167f231
Aarti Kushwaha <rtkush1211@gmail.com>
2023-01-17 22:39:06 +0530

    last try for today

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-app.listen("5000", () => {
-  console.log("Server running on port 5000...");
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
